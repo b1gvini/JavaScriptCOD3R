83079344507a0282ac1e6996660b67398d5a4998
b1gvini <vgf.vinicius@gmail.com>
2020-02-12 14:36:12 -0300

    18 a 21

--- Fundamentos/numerosAlgunsCuidados.js
+++ Fundamentos/numerosAlgunsCuidados.js
@@ -1,4 +1,6 @@
 console.log(8 / 0)
 console.log("10" / 2)
+console.log("10"+ 2) //nesse caso irá concatenar
+console.log("10"-2) //já que nao faz sentido subtrair string, converterá para int e subtrairá.
 console.log("Show!" * 2)
 console.log(0.1 + 0.7)
---
+++ Fundamentos/string.js
@@ -0,0 +1,10 @@
+const escola = "Cod3r"
+console.log(escola.charAt(4))
+console.log(escola.charAt(5)) // retorna vazio - Não dá erro.
+console.log(escola.indexOf("3"))
+console.log(escola.substring(1))
+console.log(escola.substring(0,3))
+console.log("Escola ".concat(escola).concat("!"))
+console.log(escola.replace(3, "e"))
+console.log("vinicius","gomes","de","franca")
+console.log("Ana,Maria,Paula".split(","))
